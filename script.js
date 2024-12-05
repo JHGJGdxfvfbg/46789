bda776d395c1f5a9cfc68817c1fbf4c6a6536a41
Igorka-Polishuk <igor1990polisukk@gmail.com>
2024-12-05 18:44:47 +0200

    05.12.2024

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 const URL = 'https://api.npoint.io/8758bff9f8042ca8f3aa/';
 
+let basketList = [];
+let products = [];
+
+function addProducts(data) {
+    products = data;
+}
+
 $.ajax(URL + "/products", {
     dataType: "json",
     success: (result) => {
+        addProducts(result);
+
         $.each(result, (index, element) => {
             $(".container").append(
                 `<div class="product">
                     <h3>${element.name}</h3>
-                    <p>Description: ${element.discription}</p>
                     <img src="${element.image_url}">
+                    <p>Description: ${element.discription}</p>
+                    <p>Price: ${element.price}грн</p>
+                    <button onclick="addProductToBasket(${element.id})">Buy</button>
                 </div>`
             );
         });
     },
 });
+
+function addProductToBasket(id) {
+    basketList.push(
+        products.find((product) => {
+            return product.id == id;
+        })
+    )
+
+    reDrawBasket();
+}
+
+function reDrawBasket() {
+    $(".list").html('');
+
+    let totalPrice = 0;
+
+    $.each(basketList, (index, element) => {
+        $(".list").append(`
+            <p>${element.name} | ${element.price}грн</p>
+        `);
+        totalPrice += element.price;
+    });
+
+    $(".list").append(`
+        <h2>Total Price: ${totalPrice}</h2>    
+    `);
+}
