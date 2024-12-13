568b0dcf95b97e444bc5f0fd8383d0017a89aca8
Igorka-Polishuk <igor1990polisukk@gmail.com>
2024-12-13 18:40:07 +0200

    server-db

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-const URL = 'https://api.npoint.io/8758bff9f8042ca8f3aa/';
+// 675b1a8ee1db924442971b09
 
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://marketplace-20d0.restdb.io/rest/products",
+    "method": "GET",
+    "headers": {
+        "content-type": "application/json",
+        "x-apikey": "675b1a8ee1db924442971b09",
+        "cache-control": "no-cache"
+    }
+};
 let basketList = [];
 let products = [];
+let sizeOfBasket = 0;
+
+setInterval(() => {
+    if (basketList.length === 0) {
+        $(".list").html('');
+        $(".list").html(
+            '<h1>No ready products</h1>'
+        );
+    }
+}, 10);
 
 function addProducts(data) {
     products = data;
 }
 
-$.ajax(URL + "/products", {
-    dataType: "json",
-    success: (result) => {
-        addProducts(result);
-
-        $.each(result, (index, element) => {
-            $(".container").append(
-                `<div class="product">
+$.ajax(settings).done(function (result) {
+    addProducts(result);
+    $.each(result, (index, element) => {
+        $(".container").append(
+            `<div class="product">
                     <h3>${element.name}</h3>
-                    <img src="${element.image_url}">
+                    <img src="${element.photo_url}">
                     <p>Description: ${element.discription}</p>
                     <p>Price: ${element.price}грн</p>
-                    <button onclick="addProductToBasket(${element.id})">Buy</button>
+                    <button onclick="addProductToBasket('${element._id}')">Buy</button>
                 </div>`
-            );
-        });
-    },
+        );
+    });
 });
 
 function addProductToBasket(id) {
     basketList.push(
         products.find((product) => {
-            return product.id == id;
+            return product._id == id;
         })
     )
 
@@ -43,12 +60,22 @@ function reDrawBasket() {
 
     $.each(basketList, (index, element) => {
         $(".list").append(`
-            <p>${element.name} | ${element.price}грн</p>
+            <li id="${index}"><p>${element.name} | ${element.price}грн <button class="delete-product" onclick="deleteReadyProduct(${index})">Delete</button></p></li>
         `);
         totalPrice += element.price;
+        sizeOfBasket += 1;
     });
 
     $(".list").append(`
         <h2>Total Price: ${totalPrice}</h2>    
     `);
+}
+
+function deleteReadyProduct(id) {
+    $(`#${id}`).remove();
+
+    basketList.splice(id, 1);
+    console.log(basketList);
+
+    reDrawBasket();
 }
